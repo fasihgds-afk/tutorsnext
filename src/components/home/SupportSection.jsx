d7da7cce0bedc8e90e6e27ca1416656176c66a0c
muhammadsaqlaingds-rgb <fasihgds@gmail.com>
2026-09-17 04:40:21 +0500

    missing image in SupportSection componen

--- src/components/home/SupportSection.jsx
+++ src/components/home/SupportSection.jsx
@@ -102,7 +102,7 @@ const SupportSection = () => {
             {/* Right: Agent Illustration */}
             <div className="lg:col-span-5 flex justify-center items-center">
               <img
-                src="/images/CTA-3.png"
+                src="/images/CTA-3-removebg-preview.png"
                 alt="Academic specialist"
                 className="w-full max-w-[400px] h-auto object-contain"
               />
